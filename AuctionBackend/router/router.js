73be326abc802c94f047c2920cc8b04132249288
edison.esguerra <edisonesguerra.developer@gmail.com>
2022-09-11 20:08:10 +0800

    reset files

--- AuctionBackend/router/router.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auctionController= require('../controller/auction');
-
-router.use('/auction', auctionController)
-
-
-
-module.exports = router;
--- AuctionBackend/scripts/generate_sequelize.bat
+++
@@ -1,2 +0,0 @@
-
-sequelize-auto -h localhost    -o ../model -e sqlite -c ./config/options.json
